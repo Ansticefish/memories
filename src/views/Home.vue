42d3fc5cb99db52f628dbc5c606584f6c11499cf
Ansticefish <ansticefish0106@gmail.com>
2022-12-13 14:37:18 +0800

    Reset store data and use mockdata to test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -47,10 +47,15 @@
 </template>
 
 <script>
-import paintingOne from '../assets/sea.jpg'
-import paintingTwo from '../assets/mountain.jpg'
-import paintingThree from '../assets/badminton.jpg'
-import finalPainting from '../assets/home.jpg'
+const mockData = {
+  home:
+  {
+    step0: { status: "initial", painting: "https://bnz06pap004files.storage.live.com/y4mBlLS-evahbHCnlBiOskoYBkrC4BewYNbIHxSrkYu8ixz38TKpuMkAShPLNB2RtaRoap4mQlhapVxbat2hZ2Dexli8nQlyKeigfAEMBu79s7ED5uPG0vDaprWn6TjLFl8dExS1kUFwmUR_AZgL99rUKXqTi8cUsvY60HPfZCWj2MCUNfEvW4OiK4IN0D3qqxs?width=305&height=660&cropmode=none", paintingTitle: "海的回憶" },
+    step1: { status: "sea", painting: "https://bnz06pap004files.storage.live.com/y4mai-j1nw2OE_0V-lKLi4EeSL3VhVwJgY7Qhcaq_krHE9thxK85xH41_4a-IdIH2VBEfUwHbUnPAn6U3OONJ4V6USZge8F5q-ONSmnORxmtBBHOkwfFm1M6Cvj2Wm1GsUiayH_uyOL4c2T-3oY0lFnRBNsisSiRrrMiXyqFK2w3rxna89a7EExjmXUrG_79o0q?width=660&height=469&cropmode=none", paintingTitle: "山的回憶" },
+    step2: { status: "mountain", painting: "https://bnz06pap004files.storage.live.com/y4mTbpbE02WK3ie8aSVDCUBc7l-LH1LbUPRQB3yTL86kA-RmgtJK1_pQYa00U5eZIps0QlcDf0b0-keK82xOrWi23ApavbMWCT25fp53ZPmeTaWqDZMazZjz4hru42GOqwJ2mMhdImtvqKNoizSeFnNFVYoK_rGICJtvpYWunf6z8yPj7xpalkj0B2It4Zov6ao?width=660&height=469&cropmode=none", paintingTitle: "羽球的回憶" },
+    step3: { status: "badminton", painting: "https://bnz06pap004files.storage.live.com/y4mJL8IZmqtiYDQNbtERvaSPUo49_ri5DkLxSfykho27cq7HDn_B3Ep89D6KTL-AmPzUzOAuGVxiCLWnNfjWEF8-2Vz1FunViDRVj0DwvjmmRmBF4jFm57Yj0W4gd1F96A0SJ_jdIdsiTvJXTFpY3LTSaaDK2uCdWHmIKINdMyC-XP5uYbeSd7eI3J-0EtIskrZ?width=660&height=469&cropmode=none", paintingTitle: "慶祝的回憶" }, step4: { status: "celebration", painting: "https://bnz06pap004files.storage.live.com/y4mT1PINzzsbK3aX5EEU9btnzvkG1iCcFnTbrljYITevd9c9NL8g1wG0jopAGXw1sRc90WdqAwpEOZqVCOJ_zWBUOE1tqPv11FlzCB2sMxYS3v2fm5bv4xKlQCInzYzllOR3rHFHhwMjvF7zADL-hgvG8rPC8o9Qob_EUjmUH5u-5tb3fjQYWCeLiU-Lv8urxOr?width=660&height=469&cropmode=none", paintingTitle: "我們的回憶" }
+  }
+}
 
 export default {
   name: 'Home',
@@ -66,45 +71,33 @@ export default {
   },
   computed: {
     status() {
-      return this.$store.state.status
+      return this.$store.state.home.status
     },
     paintingTitle () {
-      return this.$store.state.paintingTitle
+      return this.$store.state.home.paintingTitle
     },
     painting () {
-      return this.$store.state.painting
+      return this.$store.state.home.painting
     }
   },
   methods: {
     toAlbum () {
       switch (this.status) {
         case 'initial':
-          console.log('initial', this.status)
           this.$router.push({ name: 'sea'})
-          this.$store.commit('setStatus', 'sea')
-          this.$store.commit('setPaintingTitle', "山的回憶")
-          this.$store.commit('setPainting', paintingOne)
+          this.$store.commit('setHome', mockData.home.step1)
           break;
         case 'sea':
-          console.log('sea', this.status)
           this.$router.push({ name: 'mountain' })
-          this.$store.commit('setStatus', 'mountain')
-          this.$store.commit('setPaintingTitle', "羽球的回憶")
-          this.$store.commit('setPainting', paintingTwo)
+          this.$store.commit('setHome', mockData.home.step2)
           break;
         case 'mountain':
-          console.log('mountain', this.status)
           this.$router.push({ name: 'badminton' })
-          this.$store.commit('setStatus', 'badminton')
-          this.$store.commit('setPaintingTitle', "慶祝的回憶")
-          this.$store.commit('setPainting', paintingThree)
+          this.$store.commit('setHome', mockData.home.step3)
           break;
         case 'badminton':
-          console.log('badminton', this.status)
           this.$router.push({ name: 'celebration' })
-          this.$store.commit('setStatus', 'celebration')
-          this.$store.commit('setPaintingTitle', "我們的回憶")
-          this.$store.commit('setPainting', finalPainting)
+          this.$store.commit('setHome', mockData.home.step4)
           break;
         case 'celebration':
           this.open = !this.open
